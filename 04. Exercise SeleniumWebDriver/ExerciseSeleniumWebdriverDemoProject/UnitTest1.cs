bdecd37d4d81c49187ac3a1c2e34d4f705bf04be
Denis Atanassov <dennis.atanasoff@gmail.com>
2024-07-09 17:56:49 +0300

    first

---
+++ 04. Exercise SeleniumWebDriver/ExerciseSeleniumWebdriverDemoProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ExerciseSeleniumWebdriverDemoProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
