b5e4d38b0c257d3f41503399268b87c5eaeeebc2
Denis Atanassov <dennis.atanasoff@gmail.com>
2024-07-10 14:14:22 +0300

    data extraction

--- 04. Exercise SeleniumWebDriver/WorkingWithWebTables/WebTablesTests.cs
+++ 04. Exercise SeleniumWebDriver/WorkingWithWebTables/WebTablesTests.cs
@@ -30,6 +30,30 @@ namespace WorkingWithWebTables
             IWebElement productsTable = driver.FindElement(By.XPath("//div[@class='contentText']//table"));
 
             ReadOnlyCollection<IWebElement> tableRows = productsTable.FindElements(By.XPath("//tbody//tr"));
+
+            //Create a CSV file to save product information:
+            //Path to save the CSV file
+            string path = System.IO.Directory.GetCurrentDirectory() + "/productinformation.csv";
+
+            if (File.Exists(path)) { 
+                File.Delete(path);
+            }
+
+            foreach (IWebElement row in tableRows)
+            {
+                ReadOnlyCollection<IWebElement> tableData = row.FindElements(By.XPath(".//td")); //search in the current row "."
+
+                foreach (var tData in tableData)
+                {
+                    string data = tData.Text;
+                    string[] productInfo = data.Split("\n");
+
+                    File.AppendAllText(path, productInfo[0].Trim() + ", " + productInfo[1].Trim() + "\n");
+                }
+            }
+
+            Assert.IsTrue(File.Exists(path));
+            Assert.IsTrue(new FileInfo(path).Length > 0);
         }
     }
 }
